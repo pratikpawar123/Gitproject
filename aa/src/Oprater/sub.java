0a7cf479738af3ff837966850ac0ecf0dcdf91ec
pratik pawar <pratikpawar334@gmail.com>
2022-01-06 16:01:43 +0530

    second commit

---
+++ aa/src/Oprater/sub.java
@@ -0,0 +1,13 @@
+package Oprater;
+
+public class sub {
+
+	public static void main(String[] args) {
+System.out.println("in sub method");
+System.out.println("pratikpawar");
+
+System.out.println("pratikpawar");
+
+	}
+
+}
